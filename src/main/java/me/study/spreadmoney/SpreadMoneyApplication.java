e1ed9adf2333aa2defea8fa5d0dd6e65e6bf044c
ham-tom <mydaham1004@gmail.com>
2023-12-12 15:10:14 +0900

    Initial commit

---
+++ src/main/java/me/study/spreadmoney/SpreadMoneyApplication.java
@@ -0,0 +1,13 @@
+package me.study.spreadmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpreadMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpreadMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/study/spreadmoney/SpreadMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.spreadmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpreadMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
